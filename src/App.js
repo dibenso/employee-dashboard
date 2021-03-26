f8054989d6610581e0646741bc764fd2ba575092
Dillon Benson <dibenso93@gmail.com>
2021-03-26 19:09:57 -0400

    add table with basic employee infromation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
+import Dashboard from "./Dashboard";
 import Error from "./Error";
-import "tailwindcss/tailwind.css";
 import "./App.css";
 
 async function fetchEmployees({ success, fail }) {
   try {
-    const response = await fetch("https://randomuser.me/api/");
+    const response = await fetch("https://randomuser.me/api/?results=100");
     const data = await response.json();
 
-    success(data);
+    if(data.error)
+      fail(data.error);
+    else
+      success(data);
   } catch(error) {
     fail(error);
   }
@@ -27,6 +30,7 @@ function App() {
           setSemployees(data);
           setFetching(false);
           setError(null);
+          console.log(data);
         },
         fail: () => {
           setError({ message: "Unable to fetch employees" });
@@ -41,6 +45,7 @@ function App() {
       <h1>Employee Dashboard</h1>
       {error && <Error error={error} />}
       {fetching && "fetching..."}
+      {employees && <Dashboard employees={employees.results} />}
     </div>
   );
 }
--- src/Error.js
+++ src/Error.js
@@ -3,5 +3,5 @@ export default function Error({ error: { message } }) {
     <>
       {message}
     </>
-  )
+  );
 }
